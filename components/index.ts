016e83443e2998dab6157a9f08bab5ac825a1f78
Emyr298 <emi.ru298@gmail.com>
2023-01-01 08:47:50 +0700

    init: project structure

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export * from './blog';
+export * from './common';
+export * from './profile';
+export * from './utils';
---
+++ components/profile/UserBox/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+export const UserBox : React.FC<Props> = ({}) => {
+  return (
+    <div>
+      <h1>Ehe Te Nandayo</h1>
+    </div>
+  );
+};
