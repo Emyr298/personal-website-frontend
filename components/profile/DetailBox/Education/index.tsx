dc48143b4f7042b1a0e01ec1c238b4164ec7c075
Emyr298 <emi.ru298@gmail.com>
2023-01-27 18:38:05 +0700

    fix: education

--- components/profile/DetailBox/Education/index.tsx
+++ components/profile/DetailBox/Education/index.tsx
@@ -6,13 +6,13 @@ type Props = {};
 export const Education : React.FC<Props> = ({}) => {
   const dummyEducation = [
     {
-      association: 'University of Indonesia',
+      affiliation: 'University of Indonesia',
       degree: 'Bachelor of Computer Science',
       time: 'July 2022 - Present',
       image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/UI_Logo.svg/1200px-UI_Logo.svg.png',
     },
     {
-      association: 'SMAN 1 Bogor',
+      affiliation: 'SMAN 1 Bogor',
       degree: 'Natural Science',
       time: 'July 2021 - July 2022',
       image: 'https://www.dbl.id/uploads/school/7513/552-SMAN_1_BOGOR.png',
@@ -29,12 +29,12 @@ export const Education : React.FC<Props> = ({}) => {
               src={education.image}
               width='100'
               height='100'
-              alt={education.association}
+              alt={education.affiliation}
             />
             <div className='flex-1 flex flex-col justify-center items-center lg:items-start'>
-              <span className='text-xl font-bold'>{education.association}</span>
+              <span className='text-xl font-bold'>{education.affiliation}</span>
               <span className='text-base'>{education.degree}</span>
-              <span className='text-base'>{education.time}</span>
+              <span className='text-sm'>{education.time}</span>
             </div>
           </div>
         ))
