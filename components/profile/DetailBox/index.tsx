f60d47278f90adb35bf5098487d1e6a9d5c80932
Emyr298 <emi.ru298@gmail.com>
2023-04-19 22:07:55 +0700

    feat: remove certifications

--- components/profile/DetailBox/index.tsx
+++ components/profile/DetailBox/index.tsx
@@ -3,8 +3,6 @@ import { AboutMe } from './AboutMe';
 import { Education } from './Education';
 import { Experience } from './Experience';
 import { Projects } from './Projects';
-import { Certifications } from './Certifications';
-import { BsPersonFill } from 'react-icons/bs';
 
 type Props = {};
 
@@ -13,7 +11,6 @@ enum Page {
   Education,
   Experience,
   Projects,
-  Certifications,
 };
 
 export const DetailBox : React.FC<Props> = ({}) => {
@@ -26,7 +23,6 @@ export const DetailBox : React.FC<Props> = ({}) => {
         <button className={`h-[40px] py-1 px-2 rounded-xl ${page === Page.Education ? 'bg-green-300' : ''}`} onClick={() => setPage(Page.Education)}>Education</button>
         <button className={`h-[40px] py-1 px-2 rounded-xl ${page === Page.Experience ? 'bg-green-300' : ''}`} onClick={() => setPage(Page.Experience)}>Experience</button>
         <button className={`h-[40px] py-1 px-2 rounded-xl ${page === Page.Projects ? 'bg-green-300' : ''}`} onClick={() => setPage(Page.Projects)}>Projects</button>
-        <button className={`h-[40px] py-1 px-2 rounded-xl ${page === Page.Certifications ? 'bg-green-300' : ''}`} onClick={() => setPage(Page.Certifications)}>Certifications</button>
       </div>
       <div className='w-full lg:h-[calc(100%-75px)] mt-[20px] text-[1.15rem] lg:overflow-y-scroll lg:p-[10px]'>
         {page === Page.AboutMe ? <AboutMe /> : null}
