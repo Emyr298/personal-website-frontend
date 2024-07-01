092471e7a85516d5cf6c6cf78beb4d2c8323ac82
Emyr298 <emi.ru298@gmail.com>
2024-07-01 09:04:24 +0700

    feat: add pages to navbar

--- components/common/NavBar/index.tsx
+++ components/common/NavBar/index.tsx
@@ -4,14 +4,22 @@ import Link from 'next/link';
 export const NavBar = () => {
   return (
     <nav className='text-[1.1rem]'>
-      <div className='absolute top-0 z-50 flex h-[52px] w-full flex-row items-center justify-between bg-white p-3 drop-shadow-md'>
-        <Link href='/' className='text-xl font-bold'>
+      <div className='absolute top-0 z-50 flex h-[52px] w-full flex-row items-center justify-between bg-white px-5 py-3 drop-shadow-md'>
+        <Link href='/' className='text-xl font-bold text-black'>
           Meervix
         </Link>
-        {/* <div className='flex flex-row gap-3 items-center'>
-          <button><span className='text-gray-600 hover:text-black'>Profile</span></button>
-          <button><span className='text-gray-600 hover:text-black'>Blog</span></button>
-        </div> */}
+        <div className='flex flex-row gap-4 items-center'>
+          <Link
+            href='/'
+          >
+            <span className='text-gray-600 hover:text-black'>Profile</span>
+          </Link>
+          <Link
+            href='/blog'
+          >
+            <span className='text-gray-600 hover:text-black'>Blog</span>
+          </Link>
+        </div>
       </div>
     </nav>
   );
